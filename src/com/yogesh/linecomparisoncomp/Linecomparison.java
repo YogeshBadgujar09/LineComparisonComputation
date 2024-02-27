80a47e9b4ebdcb5209c6d2ff8ff84965c8c8007f
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-27 22:40:48 +0530

    [ADDED] : YOGESH Line Comparison Problem Set up

---
+++ src/com/yogesh/linecomparisoncomp/Linecomparison.java
@@ -0,0 +1,11 @@
+package com.yogesh.linecomparisoncomp;
+
+public class Linecomparison {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Welcome to Line Comparison Computation ");
+	 
+	}
+
+}
