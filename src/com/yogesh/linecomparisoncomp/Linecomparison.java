21f3357acb4ef5d6e9961840be11a827a0f42937
YogeshBadgujar09 <yogeshsb18@gmail.com>
2024-02-28 21:13:45 +0530

    [ADDED] : Yogesh Calculate length of line using sqrt() function

--- src/com/yogesh/linecomparisoncomp/Linecomparison.java
+++ src/com/yogesh/linecomparisoncomp/Linecomparison.java
@@ -1,10 +1,32 @@
 package com.yogesh.linecomparisoncomp;
 
+import java.util.Scanner;
+
 public class Linecomparison {
 
 	public static void main(String[] args) {
 		
-		System.out.print("Welcome to Line Comparison Computation ");
+		System.out.println("Welcome to Line Comparison Computation ");
+		
+		int x1 ;
+		int y1 ;
+		int x2 ;
+		int y2 ;
+		double lengthOfLine ;
+		
+		System.out.println("Enter Two points , X1 & Y1 and X2 & Y2 :");
+		Scanner scanner = new Scanner(System.in);
+		
+		x1 = scanner.nextInt();
+		y1 = scanner.nextInt();
+		x2 = scanner.nextInt();
+		y2 = scanner.nextInt();
+		
+		lengthOfLine = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
+		
+		System.out.println("Length of Line Is :" + lengthOfLine);
+		
+		
 	 
 	}
 
